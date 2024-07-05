84395d3d16dc4158164a0aeb29da5299c13d8c37
Victor Chiong <cjvicro@gmail.com>
2024-07-06 00:58:09 +0800

    t

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-[#FFE9C9]`}>
-        <div className="px-[20px] py-[5px] flex justify-between items-center border-b-[1px] border-[#523636]">
-          <Image className="w-[80px] h-[auto]" src={'/wangohan_logo.png'} width={100} height={100} alt="website logo" />
+        <div className="px-[20px] w-[100%] z-[999] fixed bg-[#fefefe] pt-[5px] flex justify-between items-center border-b-[1px] border-[#523636]">
+          <Image className="w-[70px] h-[auto]" src={'/wangohan_logo.png'} width={100} height={100} alt="website logo" />
           <div className={'menu flex gap-[1rem]'}>
             <div className="self-center">
               <Image src={'/icons/notification.png'} className="self-center rounded-md h-[auto] w-[40px] relative" width={10000} height={10000}  alt="website banner" />
@@ -34,7 +34,9 @@ export default function RootLayout({
             </div>
           </div>
         </div>
-        {children}
+        <div className="pt-[65.68px]">
+          {children}
+        </div>
       </body>
     </html>
   );
