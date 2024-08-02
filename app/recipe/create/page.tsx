c224ce729c321edc347dc769e10631847712423c
Victor Chiong <cjvicro@gmail.com>
2024-08-02 22:56:05 +0800

    t

--- app/recipe/create/page.tsx
+++ app/recipe/create/page.tsx
@@ -16,7 +16,7 @@ export default function RecipeCreate() {
         <div className="flex flex-col p-[20px] gap-[20px]">
             <div className="flex justify-center items-center relative mt-[10px] mb-[30px]">
                 <h1 className="absolute top-[10px] font-semibold text-[#523636]">レシピを書く</h1>
-                <Image src={'/icons/ribbon.png'} className="h-[auto] w-[200px] sm:w-[300px] max-w-none" width={10000} height={10000}  alt="website banner" />
+                <Image src={'/icons/ribbon.png'} className="h-[auto] w-[200px] sm:w-[300px] max-w-none" width={10000} height={10000}  alt="ribbon" />
             </div>
 
             <div className="create-form-container">
