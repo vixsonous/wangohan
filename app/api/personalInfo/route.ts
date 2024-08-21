8384f85b94ee26578d5affe6b2ee2c24967eb38d
Victor Chiong <cjvicro@gmail.com>
2024-08-21 16:17:21 +0800

    test

--- app/api/personalInfo/route.ts
+++ app/api/personalInfo/route.ts
@@ -16,8 +16,10 @@ export const POST = async ( req:NextRequest) => {
 
         if(file instanceof File) {
             // await deleteFilesinFolder(folderName);
-            const url = await uploadFile(file, folderName);
-            reqInput.append('fileUrl', url);
+            // const url = await uploadFile(file, folderName);
+            // reqInput.append('fileUrl', url);
+            reqInput.append('fileUrl', '');
+
         } else {
             reqInput.append('fileUrl', '');
         }
