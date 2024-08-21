206252b4670d97c801e74c9718c1c573ce544558
Victor Chiong <cjvicro@gmail.com>
2024-08-21 20:29:55 +0800

    tasd

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -41,27 +41,31 @@ export default async function User() {
     const pets : DogData[] = userDetails.pets;
     
     return (
-        <Suspense fallback={<IndexLoading />}>
-            <div className="relative pb-[100px]">
-                <div className="user-image flex flex-col justify-center items-center mt-[30px]">
+        <div className="relative pb-[100px]">
+            <div className="user-image flex flex-col justify-center items-center mt-[30px]">
+                <Suspense fallback={<div>Loading</div>}>
                     <Image loading="lazy" src={image_url} className="rounded-[100px] w-[150px] h-[150px] relative" width={10000} height={10000}  alt="website banner" />
-                    <h1 className="text-[36px] font-bold text-[#5b5351]">{userDetails.user_codename === '' ? `Wanuser` + decryptedSession.user.user_id : userDetails.user_codename}</h1>
-                </div>
-                <div className="flex justify-center items-center relative mt-[10px] mb-[30px]">
-                    <h1 className="absolute top-[10px] font-semibold text-[#523636]">うちのわん</h1>
-                    <Image loading="lazy" src={'/icons/ribbon.png'} className="h-[auto] w-[200px] sm:w-[300px] max-w-none" width={10000} height={10000}  alt="website banner" />
-                </div>
-                <div className="pet-list p-[20px] flex flex-wrap gap-[20px] items-center">
-                    {
-                        pets.map(pet => <PetContainer key={new Date().getTime() * Math.random()} petData={pet} />)
-                    }
-                </div>
-                <TabList />
-                <div className="fixed bottom-[20px] z-[9999] right-[10px]">
-                    <Image loading="lazy" src={'/Setting/settingpaw.png'} className="h-[auto] w-[120px]  max-w-none" width={10000} height={10000}  alt="website banner" />
-                    <span className="relative bottom-[33px] text-[13px] text-white right-[-45px]">設定</span>
-                </div>
+                </Suspense>
+                <h1 className="text-[36px] font-bold text-[#5b5351]">{userDetails.user_codename === '' ? `Wanuser` + decryptedSession.user.user_id : userDetails.user_codename}</h1>
             </div>
-        </Suspense>
+            <div className="flex justify-center items-center relative mt-[10px] mb-[30px]">
+                <h1 className="absolute top-[10px] font-semibold text-[#523636]">うちのわん</h1>
+                <Suspense fallback={<div>Loading</div>}>
+                    <Image loading="lazy" src={'/icons/ribbon.png'} className="h-[auto] w-[200px] sm:w-[300px] max-w-none" width={10000} height={10000}  alt="website banner" />
+                </Suspense>
+            </div>
+            <div className="pet-list p-[20px] flex flex-wrap gap-[20px] items-center">
+                {
+                    pets.map(pet => <PetContainer key={new Date().getTime() * Math.random()} petData={pet} />)
+                }
+            </div>
+            <TabList />
+            <div className="fixed bottom-[20px] z-[9999] right-[10px]">
+                <Suspense fallback={<div>Loading</div>}>
+                    <Image loading="lazy" src={'/Setting/settingpaw.png'} className="h-[auto] w-[120px]  max-w-none" width={10000} height={10000}  alt="website banner" />
+                </Suspense>
+                <span className="relative bottom-[33px] text-[13px] text-white right-[-45px]">設定</span>
+            </div>
+        </div>
     )
 }
