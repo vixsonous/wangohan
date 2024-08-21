7857f962295de42a177c9cd08f70cf470495b653
Victor Chiong <cjvicro@gmail.com>
2024-08-21 20:45:15 +0800

    back

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -18,6 +18,15 @@ type Props = {
     searchParams: {[key: string]: string | string[] | undefined}
 }
 
+export async function generateMetadata({params} : Props, parent: ResolvingMetadata):Promise<Metadata> {
+    const {userId} = params;
+    const title = `User ${userId}`;
+
+    return {
+        title: title
+    }
+}
+
 export default async function User() {
 
     const docCookies = cookies();
@@ -28,12 +37,31 @@ export default async function User() {
     const decryptedSession = await decrypt(session as string);
     const userDetails = await getUserDetails(decryptedSession.user.user_id).catch(() => redirect("/signup/personal-info"));
 
-    // const image_url = userDetails.user_image === '' ? `/recipe-making/pic-background.png` : /*await getFile(userDetails.user_image)*/ '/recipe-making/pic-background.png';
-    // const pets : DogData[] = userDetails.pets;
+    const image_url = userDetails.user_image === '' ? `/recipe-making/pic-background.png` : /*await getFile(userDetails.user_image)*/ '/recipe-making/pic-background.png';
+    const pets : DogData[] = userDetails.pets;
     
     return (
         <Suspense fallback={<IndexLoading />}>
-            asdasdasd
+            <div className="relative pb-[100px]">
+                <div className="user-image flex flex-col justify-center items-center mt-[30px]">
+                    <Image loading="lazy" src={image_url} className="rounded-[100px] w-[150px] h-[150px] relative" width={10000} height={10000}  alt="website banner" />
+                    <h1 className="text-[36px] font-bold text-[#5b5351]">{userDetails.user_codename === '' ? `Wanuser` + decryptedSession.user.user_id : userDetails.user_codename}</h1>
+                </div>
+                <div className="flex justify-center items-center relative mt-[10px] mb-[30px]">
+                    <h1 className="absolute top-[10px] font-semibold text-[#523636]">うちのわん</h1>
+                    <Image loading="lazy" src={'/icons/ribbon.png'} className="h-[auto] w-[200px] sm:w-[300px] max-w-none" width={10000} height={10000}  alt="website banner" />
+                </div>
+                <div className="pet-list p-[20px] flex flex-wrap gap-[20px] items-center">
+                    {
+                        pets.map(pet => <PetContainer key={new Date().getTime() * Math.random()} petData={pet} />)
+                    }
+                </div>
+                <TabList />
+                <div className="fixed bottom-[20px] z-[9999] right-[10px]">
+                    <Image loading="lazy" src={'/Setting/settingpaw.png'} className="h-[auto] w-[120px]  max-w-none" width={10000} height={10000}  alt="website banner" />
+                    <span className="relative bottom-[33px] text-[13px] text-white right-[-45px]">設定</span>
+                </div>
+            </div>
         </Suspense>
     )
 }
