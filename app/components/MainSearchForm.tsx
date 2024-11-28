df26582549206d315b1c953286578e3fb72e9239
Victor Chiong <cjvicro@gmail.com>
2024-11-28 14:00:50 +0800

    changes on the layout, changes on the list of recipes

--- app/components/MainSearchForm.tsx
+++ app/components/MainSearchForm.tsx
@@ -29,7 +29,9 @@ export default function MainSearchForm({isInView, isBar=false}:{isInView:boolean
         <motion.form initial={{width: isInView ? '100%' : '10%', opacity: isInView ? 1 : 0}} animate={{width: isInView ? '100%' : '10%', opacity: isInView ? 1 : 0}} className="flex gap-[10px] opacity-0 flex md:justify-start" action="">
           <div className="relative w-full flex items-center">
             <input value={search} onChange={searchOnChange} className={`py-2 px-4 ${isBar ? 'w-[100%]': 'w-[100%]'} border border-primary-text rounded-md text-xs md:text-sm`} placeholder="キーワードで検索" type="text" />
-            <MagnifyingGlass onClick={searchFunc} size={20} className="absolute right-2"/>
+            <button onClick={searchFunc} className="absolute right-2">
+              <MagnifyingGlass size={20} />
+            </button>
           </div>
           {/* <button onClick={searchFunc} className="px-4 rounded-md bg-primary-text text-white text-xs" type="submit">検索</button> */}
         </motion.form>
--- app/recipe/list/RecipeListElement.tsx
+++ app/recipe/list/RecipeListElement.tsx
@@ -9,15 +9,14 @@ export default function RecipeListElement({recipes} : RecipeListProps) {
 
     const RecipeElements = memo(({recipe, idx}:{recipe:Number, idx: Number}) => {
       return (
-      <div key={String(idx)} className="relative flex flex-col gap-2">
-          <span className="font-bold">{String(idx)}</span>
-          <img src={'/dashboard.webp'} className="w-[100%] h-[100%] object-cover max-w-none" width={10000} height={10000} alt="website banner" />
+      <div key={String(idx)} className="relative flex flex-col gap-4">
+          <img src={'/dashboard.webp'} className="aspect-square w-[100%] h-[100%] object-cover max-w-none" width={10000} height={10000} alt="website banner" />
       </div>
       )
   });
 
     return (
-        <div className=" w-[100%] gap-2 gap-x-8 grid grid-cols-2 lg:grid-cols-5">
+        <div className=" w-[100%] gap-8 grid grid-cols-4 lg:grid-cols-5">
             {recipes.map((recipe, idx) => <RecipeElements recipe={recipe} idx={idx}/>)}
         </div>
     )
