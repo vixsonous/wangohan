b71bc45bbd78394ea23811fdf48cc2eff7fa56ba
Victor Chiong <cjvicro@gmail.com>
2024-12-30 10:18:51 +0800

    layout

--- app/components/ElementComponents/HomeNavigation.tsx
+++ app/components/ElementComponents/HomeNavigation.tsx
@@ -19,7 +19,7 @@ export default function HomeNavigation() {
       <Link href={isLoggedIn ? '': '/login'} className="text-primary-text font-semibold hover:text-[#9ba3af]">レシピを作成する</Link>
       <span className="select-none">|</span>
       <Link href={'/recipe/list/1'} className="text-primary-text font-semibold hover:text-[#9ba3af]">レシピ図鑑</Link>
-      <span className="select-none hidden md:flex">|</span>
+      <span className="select-none opacity-0 md:opacity-100">|</span>
       <Link href={'/columns'} className="text-primary-text font-semibold hover:text-[#9ba3af]">犬と食に関するコラム</Link>
       <span className="select-none">|</span>
       <Link href={isLoggedIn ? `/user/settings/${user.user_id}?=#register-pet` : '/login'} className="text-primary-text font-semibold hover:text-[#9ba3af]">愛犬登録</Link>
