0ff85dcefb616cf01ae35846fcf762a5d2c86bef
Victor Chiong <cjvicro@gmail.com>
2025-01-26 21:47:37 +0800

    test

--- app/columns/page.tsx
+++ app/columns/page.tsx
@@ -3,22 +3,10 @@ import { BlogData } from "@/constants/interface";
 import Link from "next/link";
 
 export default async function Columns() {
-  const blogData: BlogData[] = await get<"blog_columns_table", BlogData>(
-    "blog_columns_table"
-  ).findAll();
-
   return (
     <section className="mt-6">
       <h1>Columns</h1>
-      <div className="flex flex-col gap-1">
-        {blogData.map((blog, idx) => {
-          return (
-            <Link key={idx} href={"/blog/columns/" + blog.blog_id}>
-              {blog.title}
-            </Link>
-          );
-        })}
-      </div>
+      <div className="flex flex-col gap-1"></div>
     </section>
   );
 }
