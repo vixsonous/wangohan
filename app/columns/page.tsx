458742dfcd3c235ad1a42e09d82146553d029aa8
Victor Chiong <cjvicro@gmail.com>
2025-01-26 22:37:12 +0800

    test

--- app/columns/page.tsx
+++ app/columns/page.tsx
@@ -1,21 +1,24 @@
 import { get } from "@/action/common";
 import { BlogData } from "@/constants/interface";
-import { Metadata } from "next";
 import Link from "next/link";
 
-export const metadata: Metadata = {
-  title: "Columns",
-};
-
 export default async function Columns() {
-  const blogData: BlogData = await get<"blog_columns_table", BlogData>(
-    "blog_columns_table"
-  ).findEqualOne("blog_id", "1");
+  // const blogData: BlogData[] = await get<"blog_columns_table", BlogData>(
+  //   "blog_columns_table"
+  // ).findAll();
 
   return (
     <section className="mt-6">
       <h1>Columns</h1>
-      <div className="flex flex-col gap-1"></div>
+      <div className="flex flex-col gap-1">
+        {/* {blogData.map((blog, idx) => {
+          return (
+            <Link key={idx} href={"/blog/columns/" + blog.blog_id}>
+              {blog.title}
+            </Link>
+          );
+        })} */}
+      </div>
     </section>
   );
 }
