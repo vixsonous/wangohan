8a8031a2f6cc8e24950c0dcb2607b46e9272c55c
Victor Chiong <cjvicro@gmail.com>
2025-02-11 00:49:30 +0800

    changed the home navigation layout

--- app/components/ElementComponents/HomeNavigation.tsx
+++ app/components/ElementComponents/HomeNavigation.tsx
@@ -5,24 +5,52 @@ import { useAppSelector } from "@/lib/redux/hooks";
 import Link from "next/link";
 
 export default function HomeNavigation() {
-  
-  const {user, isLoggedIn} = useAppSelector(state => state.user);
+  const { user, isLoggedIn } = useAppSelector((state) => state.user);
 
-  const gotoCategories = (e:React.MouseEvent<HTMLButtonElement>) => {
+  const gotoCategories = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    doScrolling('#category', 200);
-  }
+    doScrolling("#category", 200);
+  };
   return (
-    <div className="flex text-sm md:text-base gap-2 sm:gap-4 mt-10 flex-wrap justify-center">
-      <button onClick={gotoCategories} className="text-primary-text font-semibold hover:text-[#9ba3af]">レシピを探す</button>
+    <div className="flex text-sm md:text-base gap-2 sm:gap-4 mt-10 flex-wrap justify-center w-full md:justify-between md:px-8">
+      <button
+        onClick={gotoCategories}
+        className="text-primary-text font-semibold hover:text-[#9ba3af]"
+      >
+        レシピを探す
+      </button>
       <span className="select-none">|</span>
-      <Link href={isLoggedIn ? '': '/login'} className="text-primary-text font-semibold hover:text-[#9ba3af]">レシピを作成する</Link>
+      <Link
+        href={isLoggedIn ? "" : "/login"}
+        className="text-primary-text font-semibold hover:text-[#9ba3af]"
+      >
+        レシピを作成する
+      </Link>
       <span className="select-none">|</span>
-      <Link href={'/recipe/list/1'} className="text-primary-text font-semibold hover:text-[#9ba3af]">レシピ図鑑</Link>
+      <Link
+        href={"/recipe/list/1"}
+        className="text-primary-text font-semibold hover:text-[#9ba3af]"
+      >
+        レシピ図鑑
+      </Link>
       <span className="select-none opacity-0 md:opacity-100">|</span>
-      <Link href={'/columns'} className="text-primary-text font-semibold hover:text-[#9ba3af]">犬と食に関するコラム</Link>
+      <Link
+        href={"/columns"}
+        className="text-primary-text font-semibold hover:text-[#9ba3af]"
+      >
+        犬と食に関するコラム
+      </Link>
       <span className="select-none">|</span>
-      <Link href={isLoggedIn ? `/user/settings/${user.user_id}?=#register-pet` : '/login'} className="text-primary-text font-semibold hover:text-[#9ba3af]">愛犬登録</Link>
+      <Link
+        href={
+          isLoggedIn
+            ? `/user/settings/${user.user_id}?=#register-pet`
+            : "/login"
+        }
+        className="text-primary-text font-semibold hover:text-[#9ba3af]"
+      >
+        愛犬登録
+      </Link>
     </div>
-  )
-} 
+  );
+}
